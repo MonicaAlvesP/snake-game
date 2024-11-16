19294fe5b2c268bf8487e3346c62ebb55d5a2a1d
Monica Alves Pereira <92680257+MonicaAlvesP@users.noreply.github.com>
2024-11-16 01:40:11 +0000

    :tada: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# snake-game
+# 🐍 Snake-Game
+Este é um jogo da cobrinha desenvolvido pelo professor André Noel em uma aula ao vivo no canal da Rocketseat. O objetivo do jogo é controlar a cobrinha para coletar alimentos e crescer o máximo possível sem colidir com as paredes ou com o próprio corpo.
+
+## 🌟Funcionalidades
+- Controle da cobrinha usando as setas do teclado
+
+## 🚀 Tecnologias Utilizadas
+- HTML5
+- CSS3
+- JavaScript
+
+## Como Jogar
+Para jogar, abra o arquivo `index.html` em seu navegador. Você verá o frame do jogo onde a cobrinha se moverá.
+
+<p align="center">
+  <img src="./images/frame-do-jogo.png" />
+</p>
---
+++ script.js
@@ -0,0 +1,104 @@
+const DIREITA = { x: 1, y: 0 };
+const ESQUERDA = { x: -1, y: 0 };
+const CIMA = { x: 0, y: -1 };
+const BAIXO = { x: 0, y: 1 };
+
+const boardSize = 20;
+
+const initialState = {
+    snake: [{ x: 0, y: 0 }],
+    coffee: generateCoffee(),
+    direction: DIREITA,
+    gameOver: false,
+};
+
+const board = document.getElementById("board");
+
+for (let index = 0; index < boardSize * boardSize; index += 1) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    board.appendChild(cell);
+}
+
+const desenhaTabuleiro = (state) => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => (cell.className = "cell"));
+    const [cabeca, ...cauda] = state.snake;
+    const index = cabeca.y * boardSize + cabeca.x;
+    cells[index].classList.add("snake");
+    cauda.forEach((pedaco) => {
+        const index = pedaco.y * boardSize + pedaco.x;
+        cells[index].classList.add("cauda");
+    });
+
+    const coffeeIndex = state.coffee.y * boardSize + state.coffee.x;
+    cells[coffeeIndex].classList.add("coffee");
+};
+
+const moveSnake = (snake, direction) => {
+    const newHead = {
+        x: snake[0].x + direction.x,
+        y: snake[0].y + direction.y,
+    };
+
+    newHead.x = (newHead.x + boardSize) % boardSize;
+    newHead.y = (newHead.y + boardSize) % boardSize;
+
+    return [newHead, ...snake.slice(0, -1)];
+};
+
+const checkCollision = (snake) => {
+    const [head, ...body] = snake;
+    return body.some((segment) => segment.x === head.x && segment.y === head.y);
+};
+
+function generateCoffee() {
+    return {
+        x: Math.floor(Math.random() * boardSize),
+        y: Math.floor(Math.random() * boardSize),
+    };
+}
+
+const gameLoop = (state) => {
+    if (state.gameOver) {
+        alert("Game over!");
+        return;
+    }
+
+    const newSnake = moveSnake(state.snake, initialState.direction);
+    if (checkCollision(newSnake)) {
+        state.gameOver = true;
+    }
+
+    let novoCoffee = state.coffee;
+    if (newSnake[0].x === state.coffee.x && newSnake[0].y === state.coffee.y) {
+        newSnake.push({ ...newSnake[newSnake.length - 1] });
+        novoCoffee = generateCoffee();
+    }
+
+    const newState = {
+        ...state,
+        snake: newSnake,
+        coffee: novoCoffee,
+    };
+
+    desenhaTabuleiro(newState);
+    setTimeout(() => gameLoop(newState), 200);
+};
+
+const handleKeyPress = (event) => {
+    const keyMap = {
+        ArrowRight: DIREITA,
+        ArrowLeft: ESQUERDA,
+        ArrowUp: CIMA,
+        ArrowDown: BAIXO,
+    };
+
+    if (keyMap[event.key]) {
+        initialState.direction = keyMap[event.key];
+    }
+};
+
+document.addEventListener("keydown", handleKeyPress);
+desenhaTabuleiro(initialState);
+gameLoop(initialState);
